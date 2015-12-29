810e8be4cd80704e46d39c99f63fcc3a04b7ca9e
Hengne <Hengne.Li@CERN.CH>
2015-12-29 18:12:13 +0100

    update json in loadSamples.py

--- CMGTools/XZZ2l2nu/python/samples/loadSamples.py
+++ CMGTools/XZZ2l2nu/python/samples/loadSamples.py
@@ -94,7 +94,7 @@ dataSamples=SingleMuon+SingleElectron
 
 # JSON
 silverJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_Silver.txt'
-goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+goldenJson = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON_v2.txt'
 
 jsonFile = goldenJson
 
