bc4f8f9b37095133b8a40b6e707ccc5b5ee2c589
Hengne <Hengne.Li@CERN.CH>
2015-12-30 05:14:08 +0100

    keep the copy.sh scripts

--- CMGTools/XZZ2l2nu/cfg/sync/copy.sh
+++ CMGTools/XZZ2l2nu/cfg/sync/copy.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-#channel="mu"
-channel="el"
+channel="mu"
+#channel="el"
 tag="_v2"
 masses="800 1000 1200 1400 1600 1800 2000 2500 3000 3500 4000 4500"
 
 for mass in $masses;
 do
-  left="2ljets/new_list_BulkGrav_M-${mass}_${channel}Channel.txt"
+  left="../sync_${channel}${tag}/BulkGravToZZToZlepZhad_narrow_${mass}/XZZDumpEvtList/eventlist.txt"
   right="sync_${channel}${tag}/eventlist_BulkGravToZZToZlepZhad_narrow_${mass}.txt"
   echo "# ${channel} channel, M ${mass} :"
-  diff -w ${left} ${right}
+  cp $left $right
 done
 
 
