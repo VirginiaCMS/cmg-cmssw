6206e47e0246dc9a2753fac5243260e4f9401685
Hengne <Hengne.Li@CERN.CH>
2015-12-31 03:31:11 +0100

    add Zll NNLO inclusive sample

--- CMGTools/XZZ2l2nu/python/samples/samples_13TeV_RunIISpring15MiniAODv2.py
+++ CMGTools/XZZ2l2nu/python/samples/samples_13TeV_RunIISpring15MiniAODv2.py
@@ -13,6 +13,9 @@ DYJetsToLL_M50_HT200to400 = kreator.makeMCComponent("DYJetsToLL_M50_HT200to400",
 DYJetsToLL_M50_HT400to600 = kreator.makeMCComponent("DYJetsToLL_M50_HT400to600", "/DYJetsToLL_M-50_HT-400to600_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v2/MINIAODSIM", "CMS", ".*root",5.678*1.23) 
 DYJetsToLL_M50_HT600toInf = kreator.makeMCComponent("DYJetsToLL_M50_HT600toInf", "/DYJetsToLL_M-50_HT-600toInf_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM", "CMS", ".*root",2.198*1.23)
 
+# DY inclusive, NNLO 
+DYJetsToLL_M50_NNLO = kreator.makeMCComponent("DYJetsToLL_M50_NNLO", "/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM", "CMS", "*.root", 6025.2)
+
 ### DiBosons
 
 # cross section from https://twiki.cern.ch/twiki/bin/view/CMS/SummaryTable1G25ns#Diboson
